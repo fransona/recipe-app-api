de0767b7ff1cbb5b1f962f8150b3262474abfae8
fransona <jsnfrancis5@gmail.com>
2023-03-16 18:02:10 +1100

    Adding core app with Wait_for_db docker management command. Updated docker-compose and check file to have wait_for_db called before database migration.

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator funtions
+"""
+
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x+y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y-x
