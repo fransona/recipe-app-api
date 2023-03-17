dc1fb22601309347784564bbb2ab26fd5957538d
fransona <jsnfrancis5@gmail.com>
2023-03-17 22:59:36 +1100

    Fixed lint error

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,9 +11,11 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
+
 
 class PublicUserApiTests(TestCase):
     """Test the public feature of the user API"""
@@ -39,7 +41,7 @@ class PublicUserApiTests(TestCase):
 
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists"""
-        payload= {
+        payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'Test name',
@@ -51,12 +53,14 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short_error(self):
         """Test an error is returned if password less thn 5 chars"""
-        payload= {
+        payload = {
             'email': 'test@example.com',
             'password': 'pw',
             'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+            ).exists()
         self.assertFalse(user_exists)
