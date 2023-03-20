454d43595e99f14ed3fd2b1e2a5eca57ba02e5cd
fransona <jsnfrancis5@gmail.com>
2023-03-20 16:31:49 +1100

    Added Ingredient Model. Added test to create ingredients. Added serializers, view and generated url using router api for ingredients and linked to views. Added tests and implemented custom function in recipe serializer to create and update nested serializer ingredients. Finally refactored code to have base class for recipe attribute viewsets to accomodate redundant code in both

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -16,6 +16,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 # app name used in the reverse lookup of url
 app_name = 'recipe'
