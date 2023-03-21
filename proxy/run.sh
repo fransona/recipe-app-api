f8d3614e18f83135668e22dd44d3b5ae540800e6
fransona <jsnfrancis5@gmail.com>
2023-03-21 14:51:39 +1100

    Files for deployment with uwsgi and nginx configs and docker for deployment

---
+++ proxy/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Substituting the ngnix default configurations with the one we created
+# the EVN variables will be replaced with the nginx server env variables' values
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
+# Will start the ngnix run foreground
+nginx -g 'deamin off;'
